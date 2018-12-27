a4612140e89d18f6aab6b5546c9db23336207ba9
Orestis Panagopoulos <46169788+orestispanago@users.noreply.github.com>
2018-12-27 19:15:13 +0200

    Add files via upload

---
+++ esp32_bme280_mqtt.ino
@@ -0,0 +1,275 @@
+// Sends BME280 values to MQTT broker
+
+#include <WiFi.h>
+#include <PubSubClient.h>
+#include<Wire.h>
+ 
+ // BME280 I2C address is 0x76(108)
+// Changed to 0x77 for chinese clone
+#define Addr 0x77
+
+const char* ssid = "YourSSID";
+const char* password = "YourPassword";
+const char* mqttServer = "YourMQTTserverIP";
+const int mqttPort = 1883;
+const char* mqttUser = "YourNodeRedusername";
+const char* mqttPassword = "YourNodeRedPassword";
+
+
+WiFiClient espClient;
+PubSubClient client(espClient);
+
+//void callback(char* topic, byte* payload, unsigned int length) {
+// 
+//  Serial.print("Message arrived in topic: ");
+//  Serial.println(topic);
+//  String messageTemp;
+//
+//  Serial.print("Message:");
+//  for (int i = 0; i < length; i++) {
+//    Serial.print((char)payload[i]);
+//    messageTemp += (char)payload[i];
+//
+//  }
+//  Serial.println();
+//  if(messageTemp == "on"){
+//    digitalWrite(2, HIGH);
+//    Serial.println("Status: " + messageTemp);
+//  }
+//  if(messageTemp == "off"){
+//  digitalWrite(2, LOW);
+//  Serial.println("Status: "+ messageTemp);
+//
+//}
+//
+//  Serial.println();
+//  Serial.println("-----------------------");
+//}
+void setup() {
+
+// Initialise I2C communication as MASTER
+  Wire.begin();
+  Serial.begin(115200);
+ 
+  WiFi.begin(ssid, password);
+ 
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.println("Connecting to WiFi..");
+  }
+  Serial.println("Connected to the WiFi network");
+ 
+  client.setServer(mqttServer, mqttPort);
+//  client.setCallback(callback);
+ 
+  while (!client.connected()) {
+    Serial.println("Connecting to MQTT...");
+ 
+    if (client.connect("ESP32Client", mqttUser, mqttPassword )) {
+ 
+      Serial.println("connected");  
+ 
+    } else {
+ 
+      Serial.print("failed with state ");
+      Serial.print(client.state());
+      delay(2000);
+ 
+    }
+  }
+ 
+  client.subscribe("btopic");
+ 
+}
+
+void loop() {
+client.loop();
+unsigned int b1[24];
+unsigned int data[8];
+unsigned int dig_H1 = 0;
+for(int i = 0; i < 24; i++)
+{
+// Start I2C Transmission
+Wire.beginTransmission(Addr);
+// Select data register
+Wire.write((136+i));
+// Stop I2C Transmission
+Wire.endTransmission();
+ 
+// Request 1 byte of data
+Wire.requestFrom(Addr, 1);
+ 
+// Read 24 bytes of data
+if(Wire.available() == 1)
+{
+b1[i] = Wire.read();
+}
+}
+ 
+// Convert the data
+// temp coefficients
+unsigned int dig_T1 = (b1[0] & 0xff) + ((b1[1] & 0xff) * 256);
+int dig_T2 = b1[2] + (b1[3] * 256);
+int dig_T3 = b1[4] + (b1[5] * 256);
+ 
+// pressure coefficients
+unsigned int dig_P1 = (b1[6] & 0xff) + ((b1[7] & 0xff ) * 256);
+int dig_P2 = b1[8] + (b1[9] * 256);
+int dig_P3 = b1[10] + (b1[11] * 256);
+int dig_P4 = b1[12] + (b1[13] * 256);
+int dig_P5 = b1[14] + (b1[15] * 256);
+int dig_P6 = b1[16] + (b1[17] * 256);
+int dig_P7 = b1[18] + (b1[19] * 256);
+int dig_P8 = b1[20] + (b1[21] * 256);
+int dig_P9 = b1[22] + (b1[23] * 256);
+ 
+// Start I2C Transmission
+Wire.beginTransmission(Addr);
+// Select data register
+Wire.write(161);
+// Stop I2C Transmission
+Wire.endTransmission();
+ 
+// Request 1 byte of data
+Wire.requestFrom(Addr, 1);
+ 
+// Read 1 byte of data
+if(Wire.available() == 1)
+{
+dig_H1 = Wire.read();
+}
+ 
+for(int i = 0; i < 7; i++)
+{
+// Start I2C Transmission
+Wire.beginTransmission(Addr);
+// Select data register
+Wire.write((225+i));
+// Stop I2C Transmission
+Wire.endTransmission();
+ 
+// Request 1 byte of data
+Wire.requestFrom(Addr, 1);
+ 
+// Read 7 bytes of data
+if(Wire.available() == 1)
+{
+b1[i] = Wire.read();
+}
+}
+ 
+// Convert the data
+// humidity coefficients
+int dig_H2 = b1[0] + (b1[1] * 256);
+unsigned int dig_H3 = b1[2] & 0xFF ;
+int dig_H4 = (b1[3] * 16) + (b1[4] & 0xF);
+int dig_H5 = (b1[4] / 16) + (b1[5] * 16);
+int dig_H6 = b1[6];
+ 
+// Start I2C Transmission
+Wire.beginTransmission(Addr);
+// Select control humidity register
+Wire.write(0xF2);
+// Humidity over sampling rate = 1
+Wire.write(0x01);
+// Stop I2C Transmission
+Wire.endTransmission();
+ 
+// Start I2C Transmission
+Wire.beginTransmission(Addr);
+// Select control measurement register
+Wire.write(0xF4);
+// Normal mode, temp and pressure over sampling rate = 1
+Wire.write(0x27);
+// Stop I2C Transmission
+Wire.endTransmission();
+ 
+// Start I2C Transmission
+Wire.beginTransmission(Addr);
+// Select config register
+Wire.write(0xF5);
+// Stand_by time = 1000ms
+Wire.write(0xA0);
+// Stop I2C Transmission
+Wire.endTransmission();
+ 
+for(int i = 0; i < 8; i++)
+{
+// Start I2C Transmission
+Wire.beginTransmission(Addr);
+// Select data register
+Wire.write((247+i));
+// Stop I2C Transmission
+Wire.endTransmission();
+ 
+// Request 1 byte of data
+Wire.requestFrom(Addr, 1);
+ 
+// Read 8 bytes of data
+if(Wire.available() == 1)
+{
+data[i] = Wire.read();
+}
+}
+ 
+// Convert pressure and temperature data to 19-bits
+long adc_p = (((long)(data[0] & 0xFF) * 65536) + ((long)(data[1] & 0xFF) * 256) + (long)(data[2] & 0xF0)) / 16;
+long adc_t = (((long)(data[3] & 0xFF) * 65536) + ((long)(data[4] & 0xFF) * 256) + (long)(data[5] & 0xF0)) / 16;
+// Convert the humidity data
+long adc_h = ((long)(data[6] & 0xFF) * 256 + (long)(data[7] & 0xFF));
+ 
+// Temperature offset calculations
+double var1 = (((double)adc_t) / 16384.0 - ((double)dig_T1) / 1024.0) * ((double)dig_T2);
+double var2 = ((((double)adc_t) / 131072.0 - ((double)dig_T1) / 8192.0) *
+(((double)adc_t)/131072.0 - ((double)dig_T1)/8192.0)) * ((double)dig_T3);
+double t_fine = (long)(var1 + var2);
+double cTemp = (var1 + var2) / 5120.0;
+double fTemp = cTemp * 1.8 + 32;
+ 
+// Pressure offset calculations
+var1 = ((double)t_fine / 2.0) - 64000.0;
+var2 = var1 * var1 * ((double)dig_P6) / 32768.0;
+var2 = var2 + var1 * ((double)dig_P5) * 2.0;
+var2 = (var2 / 4.0) + (((double)dig_P4) * 65536.0);
+var1 = (((double) dig_P3) * var1 * var1 / 524288.0 + ((double) dig_P2) * var1) / 524288.0;
+var1 = (1.0 + var1 / 32768.0) * ((double)dig_P1);
+double p = 1048576.0 - (double)adc_p;
+p = (p - (var2 / 4096.0)) * 6250.0 / var1;
+var1 = ((double) dig_P9) * p * p / 2147483648.0;
+var2 = p * ((double) dig_P8) / 32768.0;
+double pressure = (p + (var1 + var2 + ((double)dig_P7)) / 16.0) / 100;
+ 
+// Humidity offset calculations
+double var_H = (((double)t_fine) - 76800.0);
+var_H = (adc_h - (dig_H4 * 64.0 + dig_H5 / 16384.0 * var_H)) * (dig_H2 / 65536.0 * (1.0 + dig_H6 / 67108864.0 * var_H * (1.0 + dig_H3 / 67108864.0 * var_H)));
+double humidity = var_H * (1.0 - dig_H1 * var_H / 524288.0);
+if(humidity > 100.0)
+{
+humidity = 100.0;
+}
+else if(humidity < 0.0)
+{
+humidity = 0.0;
+}
+ 
+// Output data to serial monitor
+//Serial.print("Temp: ");
+//Serial.println(cTemp);
+//Serial.print("Pres: ");
+//Serial.println(pressure);
+//Serial.print("RH: ");
+//Serial.println(humidity);
+//Serial.println();
+
+char cTempString[8];
+dtostrf(cTemp, 1, 2, cTempString);
+client.publish("btopic/temp", cTempString);
+char RHString[8];
+dtostrf(humidity, 1, 2, RHString);
+client.publish("btopic/rh", RHString);
+char pressString[8];
+dtostrf(pressure, 1, 2, pressString);
+client.publish("btopic/press", pressString);
+
+delay(10000);
+}
